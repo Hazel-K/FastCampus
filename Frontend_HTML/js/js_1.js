d46def24ae98e70690c5ed37ca4e880f95776655
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-03-09 11:22:24 +0900

    proceeding

---
+++ Frontend_HTML/js/js_1.js
@@ -0,0 +1,13 @@
+var x;
+console.log(x);
+x = 10;
+console.log(x);
+var y = 10, z = 10;
+console.log(y, z);
+// String(문자형), number(숫자형), Boolean(논리형)
+console.log(typeof x);
+// "문자열", '문자열'
+var str = "문자형";
+console.log(typeof str);
+// ture, false
+console.log(typeof true, typeof false);
