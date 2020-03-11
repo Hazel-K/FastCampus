7a4754e2c64032798020294aa604bbe64bdb9338
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-03-11 14:09:40 +0900

    proceeeeding

---
+++ Frontend_HTML/js/js_8.js
@@ -0,0 +1,22 @@
+// var test = document.getElementById("test");
+// function multiply(x, y){
+//     return x * y;
+// }
+// // test.innerHTML = multiply(2, 2);
+
+// var x = 0;
+// function interval(){
+//     test.innerHTML = ++x;
+// }
+// window.setInterval("interval", 1000);
+
+// function myFunc(){
+//     var x = 0;
+//     test.innerHTML = x;
+// }
+// myFunc();
+
+var multiply = function(x,y){
+    return x*y;
+}
+test.innerHTML = multiply(2,4);
